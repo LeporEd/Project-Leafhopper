90842faed30c3652a699dd973d7283e466929185
DerEherneRaecher <Tm2005@gmx.de>
2024-03-16 00:03:08 +0100

    Added Giant Enemy Spider, though still with issues

---
+++ giant_spider_enemy.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@onready var giant_spider_animations = $Giant_spider_animations
+@onready var hurtbox_2_shape = $Giant_spider_hurtbox_2/Hurtbox_2_shape
+
+var speed = 35
+var starty = position.y
+
+func _ready():
+	hurtbox_2_shape.set_deferred("disabled", true)
+
+func _physics_process(delta):
+	if position.y > starty + 500:
+		hurtbox_2_shape.set_deferred("disabled", false)
+		$Giant_spider_animations.animation = "Eat"
+		await get_tree().create_timer(3).timeout
+		hurtbox_2_shape.set_deferred("disabled", true)
+		$Giant_spider_animations.animation = "Standstill"
+		speed *= -1
+	if position.y <= starty:
+		speed = 35
+	velocity.y = speed
+	move_and_slide()
