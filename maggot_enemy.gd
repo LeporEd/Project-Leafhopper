282962b3dfd9eb25e7b2d9b4b0984e2cccf4489a
DerEherneRaecher <Tm2005@gmx.de>
2024-03-15 18:32:00 +0100

    Maggot can now walk

--- maggot_enemy.gd
+++ maggot_enemy.gd
@@ -1,5 +1,19 @@
 extends CharacterBody2D
 
 @onready var maggot_walk_animation: AnimatedSprite2D = $Maggot_walk
+@onready var maggot_hitbox: Area2D = $Maggot_Hitbox
 
-var direction = Vector2.LEFT
+var speed = -50
+
+func _ready():
+	#maggot_hitbox.body_entered.connect(_on_Maggot_Hitbox_body_entered)
+	pass
+
+func _physics_process(delta):
+	if is_on_wall ():
+		speed *= -1
+		$Maggot_walk.animation = "Walk" + str(speed)
+	velocity.x = speed
+	move_and_slide()
+	
+####func _on_Maggot_Hitbox_body_entered (body)   ######## Damage against Player
