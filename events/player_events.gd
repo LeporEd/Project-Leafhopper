61a2db3b85477b6baf7d97331b3ec8a06e703af3
Handprint3436 <163467464+Handprint3436@users.noreply.github.com>
2024-03-15 20:11:04 +0100

    Add player attack and event bus

---
+++ events/player_events.gd
@@ -0,0 +1,7 @@
+extends Node
+
+signal player_attack(weapon: int)
+signal player_died()
+
+signal hit_player(damage: int)
+
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var attack_timer: Timer = $AttackTimer
 
 
+const MAX_HEALTH = 100
 const SPEED = 175.0
 const JUMP_VELOCITY = -300.0
 const ALLOWED_JUMPS = 2
@@ -21,7 +23,8 @@ var state = {
 	is_hitted = false,
 	is_dying = false,
 	attack = -1,
-	jump_count = 0
+	jump_count = 0,
+	health = MAX_HEALTH
 }
 
 
@@ -29,7 +32,9 @@ func _physics_process(delta):
 	_update_state()
 	_face_player()
 	_set_velocity(delta)
+	
 	_handle_animation()
+	_perform_attack()
 	
 	move_and_slide()
 	
@@ -39,16 +44,18 @@ func _update_state():
 	state.move_x = _get_move_x()
 	state.move_y = _get_move_y()
 	state.is_running = state.move_x != MoveX.NONE
+	state.attack = _get_attack()
+	
 	
 	if is_on_floor():
 		state.jump_count = 0
 	
-	if Input.is_action_just_pressed("ui_text_backspace"):
+	if Input.is_action_just_pressed("restart"):
 		state.is_dying = true
 
 
 func _get_move_x() -> MoveX:
-	var direction_as_number: int = Input.get_axis("ui_left", "ui_right")
+	var direction_as_number: int = Input.get_axis("move_left", "move_right")
 	var direction_as_enum: MoveX
 	
 	match direction_as_number:
@@ -63,7 +70,6 @@ func _get_move_x() -> MoveX:
 
 
 func _get_move_y() -> MoveY:
-	var jump_btn_pressed = Input.is_action_just_pressed("ui_accept")
 	var directionY: MoveY = MoveY.NONE
 	var vel_dir = sign(velocity.y)
 	
@@ -74,6 +80,19 @@ func _get_move_y() -> MoveY:
 
 	return directionY
 
+func _get_attack() -> int:
+	var attack = -1
+	
+	if Input.is_action_just_pressed("attack1"):
+		attack = 1
+	elif Input.is_action_just_pressed("attack2"):
+		attack = 2
+	elif Input.is_action_just_pressed("attack3"):
+		attack = 3
+	elif Input.is_action_just_pressed("attack4"):
+		attack = 4
+	
+	return attack
 
 func _face_player():
 	if state.move_x == MoveX.NONE:
@@ -89,7 +108,7 @@ func _set_velocity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	var jump_btn_pressed = Input.is_action_just_pressed("ui_accept")
+	var jump_btn_pressed = Input.is_action_just_pressed("jump")
 	
 	if jump_btn_pressed and state.jump_count < ALLOWED_JUMPS:
 		velocity.y = JUMP_VELOCITY
@@ -99,6 +118,14 @@ func _set_velocity(delta):
 		velocity.x = state.move_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+func _perform_attack():
+	if state.attack <= 0:
+		return
+	
+	PlayerEvents.player_attack.emit(state.attack)
+	attack_timer.start()
 
 
 func _handle_animation():
@@ -123,8 +150,12 @@ func _handle_animation():
 	if state.is_dying:
 		animation = "death"
 	
-	print("Animation:" + animation)
+	print(attack_timer.time_left)
 	
-	animation_player.play(animation)
+	if not attack_timer.time_left > 0:
+		print("Animation:" + animation)
+		animation_player.play(animation)
 	
 	
+
+
