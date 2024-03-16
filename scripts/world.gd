9d58ede14f41eb90abd7e513214b293d10ec7cd9
DerEherneRaecher <Tm2005@gmx.de>
2024-03-16 20:10:45 +0100

    Battle Theme!

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
+@onready var game_theme = $Game_theme
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	game_theme.finished.connect(_on_loop_sound)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +12,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_text_backspace"):
 		PlayerEvents.hit_player.emit(25)
 	pass
+
+func _on_loop_sound():
+	game_theme.stream_paused = false
+	game_theme.play()
