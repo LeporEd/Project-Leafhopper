a5ad85fba51b827ca69705d218282b9219b59f2b
DerEherneRaecher <Tm2005@gmx.de>
2024-03-16 19:52:35 +0100

    Beetle damage setup

--- beetle_enemy.gd
+++ beetle_enemy.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 const DeathParticles = preload("res://death_explosion.tscn")
 
 var speed = -200
+var health = 2
 
 func _ready():
 	beetle_hitbox.body_entered.connect(_on_beetle_hitbox_body_entered)
@@ -23,7 +24,24 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func _on_beetle_hitbox_body_entered (body):
-	_die()
+	health = health - 1
+	if health > 0:
+		var temp = speed
+		if speed < 0:
+			$Beetle_charge.animation = "DamageLeft"
+		else:
+			$Beetle_charge.animation = "DamageRight"
+		speed = speed * -2
+		await get_tree().create_timer(1).timeout
+		speed = 0
+		await get_tree().create_timer(0.5).timeout
+		speed = temp
+		if speed < 0:
+			$Beetle_charge.animation = "ChargeLeft"
+		else:
+			$Beetle_charge.animation = "ChargeRight"
+	else:
+		_die()
 
 func _die():
 	var particles = DeathParticles.instantiate()
