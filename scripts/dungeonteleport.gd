7e18fe8c6fb60201abdfbcfeb580c22f05f47991
LeporEd <113819526+LeporEd@users.noreply.github.com>
2024-03-17 14:21:17 +0100

    Animations for teleportation gate completed

--- scripts/dungeonteleport.gd
+++ scripts/dungeonteleport.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var teleportAnim = get_node("AnimationPlayer")
 @onready var animSprite = get_node("Sprite2D3")
+@onready var secondAnimTimer = get_node("Timer")
 
 func _ready():
 	animSprite.visible = false
@@ -9,3 +10,9 @@ func _ready():
 func _on_body_entered(body):
 	#PlayerEvents.player_teleport.emit(position)
 	teleportAnim.play("teleport_on")
+	secondAnimTimer.start()
+
+
+
+func _on_timer_timeout():
+	teleportAnim.play("teleport_off")
