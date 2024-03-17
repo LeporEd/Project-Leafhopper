16509ad59cf21f8fc56f579d7807243130115fbf
DerEherneRaecher <Tm2005@gmx.de>
2024-03-17 18:16:00 +0100

    Beetle fixes

--- beetle_enemy.gd
+++ beetle_enemy.gd
@@ -2,30 +2,30 @@ extends CharacterBody2D
 
 @onready var beetle_charge_animation: AnimatedSprite2D = $Beetle_charge
 @onready var beetle_hitbox: Area2D = $Beetle_hitbox
+@onready var dazed_timer = $Dazed_Timer
+@onready var beetle_collision = $Beetle_hitbox/beetle_collision
 
 const DeathParticles = preload("res://death_explosion.tscn")
 
-var speed = -150
+var speed = -160
 var health = 2
+var dazed = false
 
 func _ready():
 	beetle_hitbox.body_entered.connect(_on_beetle_hitbox_body_entered)
 	PlayerEvents.player_load.connect(_respawn_enemy)
 
 func _physics_process(delta):
-	if is_on_wall ():
-		speed *= -1
-		await get_tree().create_timer(2).timeout
-		if speed < 0:
-			$Beetle_charge.animation = "ChargeLeft"
-		else:
-			$Beetle_charge.animation = "ChargeRight"
+	if is_on_wall () && dazed == false:
+		dazed = true
+		dazed_timer.start(2)
 	velocity.x = speed
 	move_and_slide()
 	
 func _on_beetle_hitbox_body_entered (body):
 	health = health - 1
 	if health > 0:
+		beetle_collision.set_deferred("disabled", true)
 		var temp = speed
 		if speed < 0:
 			$Beetle_charge.animation = "DamageLeft"
@@ -40,6 +40,7 @@ func _on_beetle_hitbox_body_entered (body):
 			$Beetle_charge.animation = "ChargeLeft"
 		else:
 			$Beetle_charge.animation = "ChargeRight"
+		beetle_collision.set_deferred("disabled", false)
 	else:
 		_die()
 
@@ -52,3 +53,15 @@ func _die():
 
 func _respawn_enemy():
 	health = 2
+
+
+func _on_dazed_timer_timeout():
+	speed *= -1
+	if speed < 0:
+		$Beetle_charge.animation = "ChargeLeft"
+		position.x = position.x - 5
+	else:
+		$Beetle_charge.animation = "ChargeRight"
+		position.x = position.x + 5
+	dazed = false
+	dazed_timer.stop()
