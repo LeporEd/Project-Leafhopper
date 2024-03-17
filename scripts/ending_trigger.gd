ea8ec2e46ebc14a88d97d6415543aca9cca99967
LeporEd <113819526+LeporEd@users.noreply.github.com>
2024-03-17 18:56:14 +0100

    Added signals for triggering player ending

--- scripts/ending_trigger.gd
+++ scripts/ending_trigger.gd
@@ -6,3 +6,5 @@ func _on_body_entered(body):
 	if !endingTriggered:
 		endingTriggered = true
 		WorldEvents.on_game_ending.emit()
+		PlayerEvents.player_deactivate_movement.emit()
+		PlayerEvents.player_start_endgame_animation.emit()
