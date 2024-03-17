b26b1dd40cc81bc2149ae405ed40343ab84841d5
DerEherneRaecher <Tm2005@gmx.de>
2024-03-17 19:45:27 +0100

    Beetle fixes

--- beetle_enemy.gd
+++ beetle_enemy.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @onready var beetle_charge_animation: AnimatedSprite2D = $Beetle_charge
 @onready var beetle_hitbox: Area2D = $Beetle_hitbox
 @onready var dazed_timer = $Dazed_Timer
-@onready var beetle_collision = $Beetle_hitbox/beetle_collision
 
 const DeathParticles = preload("res://death_explosion.tscn")
 
@@ -25,22 +24,20 @@ func _physics_process(delta):
 func _on_beetle_hitbox_body_entered (body):
 	health = health - 1
 	if health > 0:
-		beetle_collision.set_deferred("disabled", true)
 		var temp = speed
 		if speed < 0:
 			$Beetle_charge.animation = "DamageLeft"
 		else:
 			$Beetle_charge.animation = "DamageRight"
 		speed = speed * -2
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.75).timeout
 		speed = 0
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.25).timeout
 		speed = temp
 		if speed < 0:
 			$Beetle_charge.animation = "ChargeLeft"
 		else:
 			$Beetle_charge.animation = "ChargeRight"
-		beetle_collision.set_deferred("disabled", false)
 	else:
 		_die()
 
@@ -53,7 +50,6 @@ func _die():
 
 func _respawn_enemy():
 	health = 2
-
 
 func _on_dazed_timer_timeout():
 	speed *= -1
